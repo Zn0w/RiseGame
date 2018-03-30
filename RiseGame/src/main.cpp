fe99179af51568f2626db7f494a24072a8b097f2
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-03-30 17:45:05 +0300

    added some code comments/documentation

--- RiseGame/src/main.cpp
+++ RiseGame/src/main.cpp
@@ -166,6 +166,37 @@ int main()
 	std::map<std::string, sf::Texture> textures = load_textures("resources/levels/level_1/textures.txt");
 	std::vector<Entity_data> entity_data = load_entities_data("resources/levels/level_1/entities.txt");
 
+	/*
+		resources/levels/level_1/textures.txt contents:
+			
+			grass_background.png
+			mario_player.png
+			red_light.png
+			flower_1.png
+			flower_2.png
+	*/
+
+	/*
+		resources/levels/level_1/entities.txt contents:
+			
+			Ground_grass;grass_background.png;0.0f;0.0f;1280.0f;720.0f;
+			Decor_small_object;flower_1.png;200;200;50;50;
+			Decor_small_object;flower_1.png;900;300;65;65;
+			Decor_small_object;flower_1.png;500;500;50;50;
+			Decor_small_object;flower_2.png;750;1000;80;80;
+			Decor_small_object;flower_2.png;300;650;80;80;
+			Test_object;red_light.png;1500.0f;800.0f;300.0f;300.0f;
+			Player;mario_player.png;450.0f;500.0f;186.0f;312.0f;
+	*/
+
+	/*
+		Those two files are the text description of the level 1.
+		textures.txt scpecifies names of all the textures used in the level.
+		entities.txt specifies all of the entities in the level: their type, texture path, coordinates, dimensions.
+		The data in those files is processed by save_load_gamedata.h (the name will be changed) in order to load
+		all of those entities and textures. Which is done with the code below.
+	*/
+
 	// Vector usage optimization
 	entities.reserve(entity_data.size());
 
