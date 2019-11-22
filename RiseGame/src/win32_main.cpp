fc6b8f40de3d8a57dca8dab0fe9f3349522e1cd6
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-11-22 22:14:25 +0300

    put win32 graphics code into a win32_graphics.h (kept in the same TU)

--- RiseGame/src/win32_main.cpp
+++ RiseGame/src/win32_main.cpp
@@ -3,77 +3,14 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#include "win32/win32_graphics.h"
+#include "win32/win32_sound.h"
+
 #include "input/gamepad.h"
 #include "game.h"
 
-#include "win32/win32_sound.h"
-
-
-struct Win32_BitmapBuffer
-{
-	int width, height;
-	BITMAPINFO info;
-	void* memory;
-	int pitch;	// in bytes
-};
-
-struct WindowDimensions
-{
-	int width, height;
-};
-
-#define BYTES_PER_PIXEL 4
-
 
 static bool running;
-static Win32_BitmapBuffer backbuffer;
-
-
-WindowDimensions getWindowDimensions(HWND window_handle)
-{
-	RECT client_rect;
-	GetClientRect(window_handle, &client_rect);
-
-	return { client_rect.right - client_rect.left, client_rect.bottom - client_rect.top };
-}
-
-
-// resizes the Device Independent Buffer (DIB) section
-static void win32_resizeFrameBuffer(Win32_BitmapBuffer* buffer, int width, int height)
-{
-	if (buffer->memory)
-	{
-		VirtualFree(buffer->memory, 0, MEM_RELEASE);
-	}
-
-	buffer->width = width;
-	buffer->height = height;
-	
-	buffer->info.bmiHeader.biSize = sizeof(buffer->info.bmiHeader);
-	buffer->info.bmiHeader.biWidth = buffer->width;
-	buffer->info.bmiHeader.biHeight = -(buffer->height);
-	buffer->info.bmiHeader.biPlanes = 1;
-	buffer->info.bmiHeader.biBitCount = 32;
-	buffer->info.bmiHeader.biCompression = BI_RGB;
-
-	int bitmap_memory_size = buffer->width * buffer->height * BYTES_PER_PIXEL;
-	buffer->memory = VirtualAlloc(0, bitmap_memory_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-
-	buffer->pitch = buffer->width * BYTES_PER_PIXEL;
-}
-
-static void win32_copyBufferToWindow(Win32_BitmapBuffer* buffer, HDC device_context, int window_width, int window_height)
-{
-	StretchDIBits(
-		device_context,
-		0, 0, window_width, window_height,
-		0, 0, buffer->width, buffer->height,
-		buffer->memory,
-		&(buffer->info),
-		DIB_RGB_COLORS,
-		SRCCOPY
-	);
-}
 
 
 LRESULT CALLBACK PrimaryWindowCallback(
